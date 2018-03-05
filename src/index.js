29e9043fbea4bf7805f69099962300c56833cdd9
Josh Hoeg <josh@revivedesignstudios.com>
2018-03-04 21:57:30 -0700

    attempt at spliting prod and dev

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './app/containers/app.jsx';
 
-
+if (process.env.NODE_ENV !== 'production') {
+ console.log('Looks like we are in development mode!');
+}
 
 render(
   <App />,
---
+++ webpack.common.js
@@ -0,0 +1,72 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const extractVendorStyles = new ExtractTextPlugin('dist/vendor.css');
+const extractAppStyles = new ExtractTextPlugin('./[name]-[hash:8].min.css');
+
+
+const PATHS = {
+  app: path.join(__dirname, "src/index.js"),
+  build: path.join(__dirname, "dist"),
+};
+
+module.exports = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: "[name]-[hash:8].min.js",
+  },
+  plugins: [
+   extractAppStyles,
+   new HtmlWebpackPlugin({
+     title: "afterlive",
+     template: "./src/index.ejs"
+   }),
+ ],
+ module: {
+   rules: [
+     { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
+     {
+       test: /\.scss$/,
+       include: [ path.resolve(__dirname, "src/assets/styles") ],
+       use: extractAppStyles.extract({
+         use: [
+           { loader: 'css-loader', options: { minimize: true } },
+           { loader: 'sass-loader' }
+         ],
+         fallback: 'style-loader'
+       })
+     },
+     {
+       test: /\.css$/,
+       use: extractVendorStyles.extract({
+         use: [
+           { loader: 'css-loader', options: { minimize: true } }
+         ],
+         fallback: 'style-loader'
+       })
+     },
+     {
+       test: /\.(ejs)$/,
+       use: {
+         loader: 'html-loader',
+       }
+     },
+     {
+       test: /\.(png|svg|jpg|gif)$/,
+       use: [
+       'file-loader'
+       ]
+     },
+     {
+       test: /\.(woff|woff2|eot|ttf|otf)$/,
+       use: [
+       'file-loader'
+       ]
+     }
+   ],
+ },
+};
